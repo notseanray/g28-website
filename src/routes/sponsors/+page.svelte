820b7219b67a75d8a2dfb346da943fa860f79e4f
Sean Ray <sean.ray@startproto.com>
2023-11-07 19:37:52 -0500

    update sponsors

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -8,9 +8,27 @@
 	<div class="flex justify-center mt-20">
 		<div class="mx-[5%] max-w-5xl">
 			<div class="flex flex-wrap justify-center mt-20">
-				<img class="w-2/3 m-5" src="/startproto.svg" alt="" />
-				<img class="w-1/3 m-5" src="/lm-logo.svg" alt="" />
-				<img class="w-1/3 m-5" src="/argosy.png" alt="" />
+				<div class="w-3/6">
+					<img class="m-5" src="/startproto.svg" alt="" />
+				</div>
+				<div class="w-1/3 m-5">
+					<img class="m-5 rounded-2xl" src="/navajo_logo.jpg" alt="" />
+				</div>
+				<div class="w-1/3 m-5">
+					<img class="m-5 w-full" src="/NASA_logo.svg" alt="" />
+				</div>
+				<div class="w-1/3 m-5 m-5">
+					<img class="w-full m-5" src="/argosy.png" alt="" />
+				</div>
+				<div class="w-1/3 m-5">
+					<img class="w-full m-5" src="/lm-logo.svg" alt="" />
+				</div>
+				<div class="w-1/6 m-5">
+					<img class="w-full m-5 rounded-lg" src="/kte_logo.webp" alt="" />
+				</div>
+				<div class="w-1/6 m-5">
+					<img class="w-full m-5" src="/farmburger.png" alt="" />
+				</div>
 				<!-- <img class="w-2/3" src="/siroco.png" alt="" /> -->
 				<!-- <img class="w-1/3" src="/cloudq.png" alt="" /> -->
 			</div>
